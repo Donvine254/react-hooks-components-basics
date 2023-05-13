fe4a3c0e23fed6fb86885440f87d7439783d2f94
donvine mugendi <donvinemugendi@gmail.com>
2023-05-14 00:05:07 +0300

    updated comment

--- src/Comment.js
+++ src/Comment.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Comment() {
-  return <div>Naturally, I agree with this article.</div>;
+  return <div><p>Jane Doe: </p><p>Naturally, I agree with this article.</p></div>;
 }
 
 export default Comment;
